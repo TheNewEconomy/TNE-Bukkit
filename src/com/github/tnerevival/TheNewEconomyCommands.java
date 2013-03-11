b123725a11a474d4dde95f13aeee1581acac82a0
Daniel V <daniel.mogpcreations@gmail.com>
2013-03-11 01:34:31 -0400

    Added the barebones of the Account class and more...

--- src/com/github/tnerevival/TheNewEconomyCommands.java
+++ src/com/github/tnerevival/TheNewEconomyCommands.java
@@ -19,34 +19,58 @@ public class TheNewEconomyCommands implements CommandExecutor {
 			Player p = (Player) sender;
 			if (cmd.getName().equalsIgnoreCase("area")) {
 				if(p.hasPermission("TNE.USER.AREA")) {
+				} else {
+					p.sendMessage("You do not have permission to do that!");
+					return false;
 				}
 			} else if (cmd.getName().equalsIgnoreCase("auction")) {
 				if(p.hasPermission("TNE.USER.AUCTION")) {
 					
+				} else {
+					p.sendMessage("You do not have permission to do that!");
+					return false;
 				}
 			} else if (cmd.getName().equalsIgnoreCase("bank")) {
 				if(p.hasPermission("TNE.USER.BANK")) {
 				
+				} else {
+					p.sendMessage("You do not have permission to do that!");
+					return false;
 				}
 			} else if (cmd.getName().equalsIgnoreCase("buy")) {
 				if(p.hasPermission("TNE.USER.ECO")) {
 					
+				} else {
+					p.sendMessage("You do not have permission to do that!");
+					return false;
 				}
 			} else if (cmd.getName().equalsIgnoreCase("economy")) {
 				if(p.hasPermission("TNE.ADMIN")) {
 					
+				} else {
+					p.sendMessage("You do not have permission to do that!");
+					return false;
 				}
 			} else if (cmd.getName().equalsIgnoreCase("lottery")) {
 				if(p.hasPermission("TNE.USER.LOTTO")) {
 					
+				} else {
+					p.sendMessage("You do not have permission to do that!");
+					return false;
 				}
 			} else if (cmd.getName().equalsIgnoreCase("money")) {
 				if(p.hasPermission("TNE.USER.ECO")) {
 					
+				} else {
+					p.sendMessage("You do not have permission to do that!");
+					return false;
 				}
 			} else if (cmd.getName().equalsIgnoreCase("offer")) {
 				if(p.hasPermission("TNE.USER.TRADE")) {
 					
+				} else {
+					p.sendMessage("You do not have permission to do that!");
+					return false;
 				}
 			}
 		}
---
+++ src/com/github/tnerevival/core/Account.java
@@ -0,0 +1,54 @@
+package com.github.tnerevival.core;
+
+import org.bukkit.entity.Player;
+
+public class Account {
+	
+	//SECTION: methods
+	
+	
+	/**
+	 * Used to create an account.
+	 * @param p
+	 */
+	public void createAccount(Player p) {
+		
+	}
+	
+	/**
+	 * Used to delete an account.
+	 * @param p
+	 */
+	public void deleteAccount(Player p) {
+		
+	}
+	
+	/**
+	 * Used to backup every account just in-case.
+	 * @param p
+	 */
+	public void backupAccounts(Player p) {
+		
+	}
+	
+	/**
+	 * Used to initialize the account directory and add a README for the account system and an example account.
+	 */
+	public void initAccountDirectory() {
+		
+	}
+	
+	
+	//SECTION: boolean
+	
+	
+	/**
+	 * Used to check if a particular account exists.
+	 * @param p
+	 * @return Whether or not the account exists.
+	 */
+	public boolean doesAccountExist(Player p) {
+		return false;
+	}
+	
+}
