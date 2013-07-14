3d5f3e43e92f685568a6eeaa4eb23e03667ccc48
TheMonkey1415 <dan@lrn2.net>
2013-07-13 17:11:16 -0700

    Added Miner Job and Miner Rank

--- src/com/github/tnerevival/core/jobs/MinerJob.java
+++ src/com/github/tnerevival/core/jobs/MinerJob.java
@@ -1,6 +1,8 @@
 package com.github.tnerevival.core.jobs;
 
 import java.awt.Color;
+import java.awt.List;
+import java.util.ArrayList;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -34,11 +36,30 @@ public class MinerJob extends Job {
 		return null;
 	}
 	
-	//todo: lists of each rank
+	public Player vicePresident() {
+		return null;
+	}
+	
+	public ArrayList<String> juniorAssociates = new ArrayList<String>();
+	public ArrayList<String> associates = new ArrayList<String>();
+	public ArrayList<String> seniorAssociates = new ArrayList<String>();
+	public ArrayList<String> managers = new ArrayList<String>();
+	public ArrayList<String> supervisor = new ArrayList<String>();
+	//public ArrayList<String> vicePresident = new ArrayList<String>(); not needed
+	//ArrayList<String> president = new ArrayList<String>(); also not needed
 	
 	public void OnJobLeaderFired(Player exLeader, Player newLeader) {
+		if (newLeader == vicePresident()) {
+			newLeader = jobLeader();
+			exLeader = vicePresident();
 		exLeader.sendMessage(ChatColor.RED + "[" + ChatColor.GREEN + "Jobs" + ChatColor.RED + "] " +
 				"You've been replaced by " + ChatColor.DARK_PURPLE + newLeader.getDisplayName() + "!");
+		newLeader.sendMessage(ChatColor.RED + "[" + ChatColor.GREEN + "Jobs" + ChatColor.RED + "] " +
+				"You've replaced " + ChatColor.DARK_PURPLE + exLeader.getDisplayName() + ChatColor.RED +
+				" as the President of Miners! Congrats!");
+		} else {
+			//not going to happen
+		}
 	}
 }
 
