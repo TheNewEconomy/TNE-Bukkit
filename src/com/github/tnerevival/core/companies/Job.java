4a4a941540f423feb2d7ab5517956a3f1f2c34d2
TheMonkey1415 <dan@lrn2.net>
2013-07-14 11:10:27 -0700

    Added employees in Job File

--- src/com/github/tnerevival/core/companies/Job.java
+++ src/com/github/tnerevival/core/companies/Job.java
@@ -1,5 +1,8 @@
 package com.github.tnerevival.core.companies;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 /**
  * 
  * @author creatorfromhell, TheMonkey1415
@@ -9,6 +12,7 @@ public abstract class Job {
 	String name;
 	String title;
 	Double paycheck;
+	ArrayList<String> employees = new ArrayList<String>();
 	
 	public Job(String name) {
 		this(name, name, 7.50);
@@ -22,6 +26,16 @@ public abstract class Job {
 		this.name = name;
 		this.title = title;
 		this.paycheck = paycheck;
+	}
+	
+	public Job(String name, String title, Double paycheck, String[] employees) {
+		this.name = name;
+		this.title = title;
+		this.paycheck = paycheck;
+		
+		for (String s : employees) {
+			this.employees.add(s);
+		}
 	}
 
 	/**
