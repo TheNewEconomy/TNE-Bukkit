ded9bc07a5975449b44a20206269dca4daf8687b
Daniel V <daniel.viddy@gmail.com>
2016-10-17 12:52:32 -0400

    More unit tests

--- src/com/github/tnerevival/commands/auction/AuctionCommand.java
+++ src/com/github/tnerevival/commands/auction/AuctionCommand.java
@@ -5,7 +5,22 @@ import com.github.tnerevival.commands.TNECommand;
 import org.bukkit.command.CommandSender;
 
 /**
- * Created by Daniel on 10/17/2016.
+ * The New Economy Minecraft Server Plugin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created by creatorfromhell on 10/17/2016.
  */
 public class AuctionCommand extends TNECommand {
 
---
+++ src/test/TNEAPITest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+
+/**
+ * Created by Daniel on 10/11/2016.
+ */
+public class TNEAPITest {
+
+  @Test
+  public void apiEnabledTest() {
+  }
+}
