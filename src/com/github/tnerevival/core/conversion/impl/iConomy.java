4e9fd855c7072132824b5ee16f4292a5fd6ba4aa
Daniel V <daniel.viddy@gmail.com>
2016-11-15 06:39:27 -0500

    iConomy account importing. Towards #85.

--- src/com/github/tnerevival/core/conversion/impl/iConomy.java
+++ src/com/github/tnerevival/core/conversion/impl/iConomy.java
@@ -16,13 +16,32 @@
  */
 package com.github.tnerevival.core.conversion.impl;
 
+import com.github.tnerevival.TNE;
+import com.github.tnerevival.account.Account;
+import com.github.tnerevival.account.IDFinder;
 import com.github.tnerevival.core.conversion.Converter;
+import com.github.tnerevival.core.currency.Currency;
+import com.github.tnerevival.core.db.MySQL;
 import com.github.tnerevival.core.exception.InvalidDatabaseImport;
+import com.github.tnerevival.core.transaction.TransactionType;
+import com.github.tnerevival.utils.AccountUtils;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.UUID;
+import java.util.logging.Level;
 
 /**
  * Created by creatorfromhell on 11/13/2016.
  **/
 public class iConomy extends Converter {
+  private File configFile = new File(TNE.instance.getDataFolder() + "../iConomy", "Config.yml");
+  private FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+  private String table = config.getString("System.Database.Table");
+
   @Override
   public String name() {
     return "iConomy";
@@ -30,12 +49,28 @@ public class iConomy extends Converter {
 
   @Override
   public void mysql() throws InvalidDatabaseImport {
-    super.mysql();
+    db = new MySQL(mysqlHost, mysqlPort, mysqlDatabase, mysqlUser, mysqlPassword);
+    try {
+      mysqlDB().executeQuery("SELECT * FROM " + table + ";");
+
+      Currency currency = TNE.instance.manager.currencyManager.get(TNE.instance.defaultWorld);
+      while (mysqlDB().results().next()) {
+        String username = mysqlDB().results().getString("username");
+        Double balance = mysqlDB().results().getDouble("balance");
+
+        UUID id = IDFinder.getID(username);
+        Account account = new Account(IDFinder.getID(username));
+        account.setBalance(TNE.instance.defaultWorld, balance, currency.getName());
+        TNE.instance.manager.accounts.put(id, account);
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
-  public void sqlite() throws InvalidDatabaseImport {
-    super.sqlite();
+  public void h2() throws InvalidDatabaseImport {
+    super.h2();
   }
 
   @Override
@@ -45,7 +80,29 @@ public class iConomy extends Converter {
 
   @Override
   public void flatfile() throws InvalidDatabaseImport {
-    super.flatfile();
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(new File("accounts.txt")));
+
+      String line;
+      while((line = reader.readLine()) != null) {
+        String[] split = line.split(" ");
+        UUID id = IDFinder.getID(split[0].trim());
+        Double money = Double.parseDouble(split[1].split(":")[1]);
+
+
+        AccountUtils.transaction(null,
+            id.toString(),
+            money,
+            TNE.instance.manager.currencyManager.get(
+                TNE.instance.defaultWorld
+            ),
+            TransactionType.MONEY_GIVE,
+            TNE.instance.defaultWorld
+        );
+      }
+    } catch(Exception e) {
+      TNE.instance.getLogger().log(Level.WARNING, "Unable to load iConomy Data.");
+    }
   }
 
   @Override
