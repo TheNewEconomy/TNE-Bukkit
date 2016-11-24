385cfa3abf75eb4246a447ffd19a615d22b88194
Daniel V <daniel.viddy@gmail.com>
2016-11-24 01:01:39 -0500

    Configurations for enabling/disabling direct-sql transactions, and caching for such.

--- src/com/github/tnerevival/core/configurations/impl/MainConfiguration.java
+++ src/com/github/tnerevival/core/configurations/impl/MainConfiguration.java
@@ -89,6 +89,9 @@ public class MainConfiguration extends Configuration {
     configurations.put("Core.Database.Prefix", "TNE");
     configurations.put("Core.Database.Backup", true);
     configurations.put("Core.Database.FlatFile.File", "economy.tne");
+    configurations.put("Core.Database.Transactions.Enabled", true);
+    configurations.put("Core.Database.Transactions.Caching.Enabled", true);
+    configurations.put("Core.Database.Transactions.Caching.Update", 600);
     configurations.put("Core.Database.MySQL.Host", "localhost");
     configurations.put("Core.Database.MySQL.Port", 3306);
     configurations.put("Core.Database.MySQL.Database", "TheNewEconomy");
