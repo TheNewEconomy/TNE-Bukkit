88ef440494e29ffbfb0a5ea2aeb126e31fe8dcde
creatorfromhell <Madmoxxymocksmemockingmorris@26047>
2017-04-08 05:41:30 -0400

    Update version to Alpha 5.5

--- src/com/github/tnerevival/core/SaveManager.java
+++ src/com/github/tnerevival/core/SaveManager.java
@@ -24,10 +24,11 @@ public class SaveManager {
     versions.put(5.2, new Alpha5_2());
     versions.put(5.3, new Alpha5_2());
     versions.put(5.4, new Alpha5_2());
+    versions.put(5.5, new Alpha5_2());
   }
 
   public Version versionInstance;
-  Double currentSaveVersion = 5.4;
+  Double currentSaveVersion = 5.5;
   Double saveVersion = 0.0;
   public boolean updating = false;
   public boolean cache = TNE.configurations.getBoolean("Core.Database.Transactions.Cache");
