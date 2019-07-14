edcfcc8a08dfbf22fd8c5948a57bf86d2a04d19d
creatorfromhell <daniel.viddy@gmail.com>
2019-07-14 01:53:02 -0400

    Change build name to work  towards next release.

--- TNE/src/net/tnemc/core/TNE.java
+++ TNE/src/net/tnemc/core/TNE.java
@@ -137,7 +137,7 @@ public class TNE extends TNELib {
   //BukkitRunnable Workers
   private SaveWorker saveWorker;
 
-  public static final String build = "1Beta118f";
+  public static final String build = "1Beta118g";
 
   private boolean blacklisted = false;
   public static boolean useMod = false;
