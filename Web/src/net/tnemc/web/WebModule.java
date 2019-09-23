0e326fa66a7d09b55969804e35d974b318112c3d
creatorfromhell <daniel.viddy@gmail.com>
2019-09-23 02:11:37 -0400

    Fix Web Module configurations.

--- Web/src/net/tnemc/web/WebModule.java
+++ Web/src/net/tnemc/web/WebModule.java
@@ -6,6 +6,12 @@ import net.tnemc.core.common.module.Module;
 import net.tnemc.core.common.module.ModuleInfo;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
 /**
  * The New Economy Minecraft Server Plugin
@@ -33,7 +39,7 @@ public class WebModule implements Module {
   public void load(TNE tne) {
     instance = this;
     webFile = new File(TNE.instance().getDataFolder(), "web.yml");
-    fileConfiguration = TNE.instance().initializeConfiguration(webFile, "web.yml");
+    fileConfiguration = initializeConfiguration(webFile, "web.yml");
     manager = new WebManager();
     TNE.logger().info("Web Module loaded!");
   }
@@ -46,7 +52,7 @@ public class WebModule implements Module {
   @Override
   public void postLoad(TNE tne) {
     try {
-      manager.start(8896);
+      manager.start(fileConfiguration.getInt("Web.Port"));
     } catch (Exception ignore) {
       ignore.printStackTrace();
       TNE.logger().warning("Failed to start Web Server on Port: " + fileConfiguration.getInt("Web.Port"));
@@ -72,5 +78,33 @@ public class WebModule implements Module {
 
   public CommentedConfiguration getFileConfiguration() {
     return fileConfiguration;
+  }
+
+  public CommentedConfiguration initializeConfiguration(File file, String defaultFile) {
+    TNE.debug("Started copying " + file.getName());
+    CommentedConfiguration commentedConfiguration = new CommentedConfiguration(file, new InputStreamReader(getResource(defaultFile), StandardCharsets.UTF_8), false);
+    TNE.debug("Initializing commented configuration");
+    if(commentedConfiguration != null) {
+      TNE.debug("Loading commented configuration");
+      commentedConfiguration.load();
+    }
+    TNE.debug("Finished copying " + file.getName());
+    return commentedConfiguration;
+  }
+
+
+  public InputStream getResource(String filename) {
+    try {
+      URL url = getClass().getClassLoader().getResource(filename);
+      if (url == null) {
+        return null;
+      } else {
+        URLConnection connection = url.openConnection();
+        connection.setUseCaches(false);
+        return connection.getInputStream();
+      }
+    } catch (IOException var4) {
+      return null;
+    }
   }
 }
