35b6c1edf7192fac791bbf9b06086ea138107c3c
creatorfromhell <daniel.viddy@gmail.com>
2019-12-12 08:12:59 -0500

    Add version checks for 1.16 and 1.17

--- TNE/src/net/tnemc/core/common/utils/MISCUtils.java
+++ TNE/src/net/tnemc/core/common/utils/MISCUtils.java
@@ -75,50 +75,68 @@ public class MISCUtils {
    * @return Whether the bukkit in use is for MC >= 1.8
    */
   public static boolean isOneSeven() {
-    return Bukkit.getVersion().contains("1.7") || isOneEight() || isOneNine() || isOneTen() || isOneEleven() || isOneTwelve() || isOneThirteen() || isOneFourteen();
+    return Bukkit.getVersion().contains("1.7") || isOneEight() || isOneNine() || isOneTen() || isOneEleven()
+        || isOneTwelve() || isOneThirteen() || isOneFourteen() || isOneFifteen() || isOneSixteen() || isOneSeventeen();
   }
 
   /**
    * @return Whether the bukkit in use is for MC >= 1.8
    */
   public static boolean isOneEight() {
-    return Bukkit.getVersion().contains("1.8") || isOneNine() || isOneTen() || isOneEleven() || isOneTwelve() || isOneThirteen() || isOneFourteen();
+    return Bukkit.getVersion().contains("1.8") || isOneNine() || isOneTen() || isOneEleven() || isOneTwelve()
+        || isOneThirteen() || isOneFourteen() || isOneFifteen() || isOneSixteen() || isOneSeventeen();
   }
 
   /**
    * @return Whether the bukkit in use is for MC >= 1.9
    */
   public static boolean isOneNine() {
-    return Bukkit.getVersion().contains("1.9") || isOneTen() || isOneEleven() || isOneTwelve() || isOneThirteen() || isOneFourteen();
+    return Bukkit.getVersion().contains("1.9") || isOneTen() || isOneEleven() || isOneTwelve() || isOneThirteen() ||
+        isOneFourteen() || isOneFifteen() || isOneSixteen() || isOneSeventeen();
   }
 
   /**
    * @return Whether the bukkit in use is for MC >= 1.10
    */
   public static boolean isOneTen() {
-    return Bukkit.getVersion().contains("1.10") || isOneEleven() || isOneTwelve() || isOneThirteen() || isOneFourteen();
+    return Bukkit.getVersion().contains("1.10") || isOneEleven() || isOneTwelve() || isOneThirteen() || isOneFourteen()
+        || isOneFifteen() || isOneSixteen() || isOneSeventeen();
   }
 
   /**
    * @return Whether the bukkit in use is for MC >= 1.11
    */
   public static boolean isOneEleven() {
-    return Bukkit.getVersion().contains("1.11") || isOneTwelve() || isOneThirteen() || isOneFourteen();
+    return Bukkit.getVersion().contains("1.11") || isOneTwelve() || isOneThirteen() || isOneFourteen() || isOneFifteen()
+        || isOneSixteen() || isOneSeventeen();
   }
 
   /**
    * @return Whether the bukkit in use is for MC >= 1.12
    */
   public static boolean isOneTwelve() {
-    return Bukkit.getVersion().contains("1.12") || isOneThirteen() || isOneFourteen();
+    return Bukkit.getVersion().contains("1.12") || isOneThirteen() || isOneFourteen() || isOneFifteen() || isOneSixteen()
+        || isOneSeventeen();
   }
 
   public static boolean isOneThirteen() {
-    return Bukkit.getVersion().contains("1.13") || isOneFourteen();
+    return Bukkit.getVersion().contains("1.13") || isOneFourteen() || isOneFifteen() || isOneSixteen() || isOneSeventeen();
   }
 
   public static boolean isOneFourteen() {
-    return Bukkit.getVersion().contains("1.14");
+    return Bukkit.getVersion().contains("1.14") || isOneFifteen() || isOneSixteen() || isOneSeventeen();
+  }
+
+  public static boolean isOneFifteen() {
+    return Bukkit.getVersion().contains("1.15") || isOneSixteen() || isOneSeventeen();
+  }
+
+  public static boolean isOneSixteen() {
+    return Bukkit.getVersion().contains("1.16") || isOneSeventeen();
+  }
+
+  public static boolean isOneSeventeen() {
+    return Bukkit.getVersion().contains("1.17");
   }
 
   public static boolean offHand() {
